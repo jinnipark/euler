c9c68feb79fa49b70fd01756b81b6b5c75ad8ca8
Sungjin Park <jinni.park@sk.com>
2013-12-12 14:02:06 +0900

    Remove sorted constraint from set.

--- src/eu_set.erl
+++ src/eu_set.erl
@@ -13,41 +13,41 @@
 %%
 %% Calculate a union of set S1 and S2.
 %%
-%% Params: list(), list() - sorted
-%% Returns: list() - sorted list
+%% Params: list(), list()
+%% Returns: list()
 %%
 union(S1, S2) ->
 	union(S1, S2, []).
 
 union([], S2, U) ->
 	U ++ S2;
-union(S1, [], U) ->
-	U ++ S1;
-union([E | S1], [E | S2], U) ->
-	union(S1, S2, U ++ [E]);
-union([E1 | S1], [E2 | S2], U) ->
-	case E1 < E2 of
-		true -> union(S1, [E2 | S2], U ++ [E1]);
-		false -> union([E1 | S1], S2, U ++ [E2])
-	end.
+union([E | S1], S2, U) ->
+	{_, S3} = take(E, S2),
+	union(S1, S3, U ++ [E]).
+
+take(E, L) ->
+	take(E, L, {undefined, []}).
+
+take(_E, [], Result) ->
+	Result;
+take(E, [E | L], {undefined, T}) ->
+	{E, T ++ L};
+take(E, [E1 | L], {undefined, T}) ->
+	take(E, L, {undefined, T ++ [E1]}).
 
 %%
 %% Calculate an intersection of set S1 and S2.
 %%
-%% Params: list(), list() - sorted
-%% Returns: list() - sorted
+%% Params: list(), list()
+%% Returns: list()
 %%
 intersection(S1, S2) ->
 	intersection(S1, S2, []).
 
 intersection([], _S2, I) ->
 	I;
-intersection(_S1, [], I) ->
-	I;
-intersection([E | S1], [E | S2], U) ->
-	intersection(S1, S2, U ++ [E]);
-intersection([E1 | S1], [E2 | S2], U) ->
-	case E1 < E2 of
-		true -> intersection(S1, [E2 | S2], U);
-		false -> intersection([E1 | S1], S2, U)
+intersection([E | S1], S2, I) ->
+	case take(E, S2) of
+		{undefined, S3} -> intersection(S1, S3, I);
+		{E, S3} -> intersection(S1, S3, I ++ [E])
 	end.
